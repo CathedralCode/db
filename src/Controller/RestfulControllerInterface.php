60b1a3acf1b028d2365187dde62e10d6d2fa94d6
Philip Michael Raab <peep@inane.co.za>
2022-01-19 22:00:08 +0200

    fix: float type missing

--- src/Controller/RestfulControllerInterface.php
+++ src/Controller/RestfulControllerInterface.php
@@ -2,7 +2,7 @@
 /**
  * Restful Controller Interface
  *
- * PHP version 7
+ * PHP version 8
  *
  * @package Cathedral\Db
  * @author Philip Michael Raab <philip@inane.co.za>
@@ -14,7 +14,7 @@ namespace Cathedral\Db\Controller;
 /**
  * Restful Controller Interface
  *
- * @version 0.2.0
+ * @version 0.3.0
  * @package Cathedral\Db
  */
 interface RestfulControllerInterface {
@@ -22,8 +22,15 @@ interface RestfulControllerInterface {
      * @var integer default page size
      */
     const PAGINATION_PAGESIZE = 10;
+
     /**
      * @var string type int
      */
     const CONTENT_TYPE_NUMBER = 'int';
+
+    /**
+     * @since 0.3.0
+     * @var string type float
+     */
+    const CONTENT_TYPE_FLOAT = 'float';
 }
